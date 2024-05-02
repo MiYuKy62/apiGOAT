27ba438595625563c4cda1ad3b6ec9c0a55a721c
MiYuKy62 <mat62142@gmail.com>
2024-05-02 10:01:21 +0200

    2

--- serveur.js
+++ serveur.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json'); // Importez votre fichier JSON Swagger ici
 const port = 3000;
@@ -6,8 +7,10 @@ const version = 'v1';
 const router = require('./routes/routes');
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use(`/api/${version}`, router);
 
